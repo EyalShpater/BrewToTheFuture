a9b139522c5affdd4db832ed778917bcbdc1bd9b
Eyal Shpater <shpaters@hotmail.com>
2024-04-05 00:28:52 +0300

    init

---
+++ src/main/java/ema/brewtothefuture/Recipe/Recipe.java
@@ -0,0 +1,17 @@
+package ema.brewtothefuture.Recipe;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Recipe {
+    int counter = 0;
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam String name) {
+        counter++;
+        System.out.println("Request number " + counter + " received");
+        return "Hello, " + name + "!";
+    }
+}
