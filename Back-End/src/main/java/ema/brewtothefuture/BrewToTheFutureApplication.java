8b3b89b137059fe7abaa7f599d94259e3567a098
Eyal Shpater <shpaters@hotmail.com>
2024-04-05 00:11:59 +0300

    init

---
+++ Back-End/src/main/java/ema/brewtothefuture/BrewToTheFutureApplication.java
@@ -0,0 +1,13 @@
+package ema.brewtothefuture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewToTheFutureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrewToTheFutureApplication.class, args);
+    }
+
+}
