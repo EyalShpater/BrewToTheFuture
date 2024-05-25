7487c74f4200dfabb2ebf309530aed851ce113ab
Eyal Shpater <shpaters@hotmail.com>
2024-05-25 16:20:56 +0300

    funny health

--- Back-End/BrewingSystem/src/main/java/ema/brewtothefuture/api/BrewingSystemController.java
+++ Back-End/BrewingSystem/src/main/java/ema/brewtothefuture/api/BrewingSystemController.java
@@ -2,12 +2,12 @@ package ema.brewtothefuture.api;
 
 import ema.brewtothefuture.dto.embedded.EmbeddedRecipeDTO;
 import ema.brewtothefuture.dto.front.RecipeDTO;
-import ema.brewtothefuture.recipe.impl.Recipe;
 import ema.brewtothefuture.system.api.BrewingSystem;
 import ema.brewtothefuture.system.impl.BrewingSystemImpl;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Random;
 
 @RestController
 public class BrewingSystemController {
@@ -16,8 +16,26 @@ public class BrewingSystemController {
 
     @GetMapping("/api/health")
     public String health() {
-        return "OK";
+        // return random dad joke from array of 3 jokes
+        List<String> sentences = List.of(
+                "Why did the scarecrow win an award? Because he was outstanding in his field!",
+                "I'm reading a book on the history of glue. I just can't seem to put it down!",
+                "Why don't scientists trust atoms? Because they make up everything!",
+                "What do you call a fish wearing a crown? A kingfish!",
+                "Why did the tomato turn red? Because it saw the salad dressing!",
+                "What do you call a factory that makes okay products? A satisfactory!",
+                "Why did the math book look sad? Because it had too many problems!",
+                "What do you call a belt made out of watches? A waist of time!",
+                "Matan Ma Lo Purim Hayom?",
+                "Adi Raza and Zodeket",
+                "Eyal HaMelech",
+                "BBZNOT");
+        Random random = new Random();
+        int index = random.nextInt(sentences.size());
+
+        return sentences.get(index);
     }
+
     @PostMapping("/api/brew/recipe")
     public void brew(@RequestBody RecipeDTO recipeDTO) {
         int recipeId = brewingSystem.addNewRecipe(recipeDTO);
