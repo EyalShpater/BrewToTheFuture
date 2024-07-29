add615450e8a792d6caca09ff061c43a12b156f2
Adi Kapuri <adikap1904@gmail.com>
2024-07-29 16:38:51 +0300

    Brew screen

--- Front-End/screens/brew/Brew.jsx
+++ Front-End/screens/brew/Brew.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./brew.style";
-import { images } from "../../constants";
+import { COLORS, images } from "../../constants";
 import { useNavigation } from "@react-navigation/native";
 import {
   SafeAreaView,
@@ -12,13 +12,49 @@ import {
   Modal,
   Button,
   FlatList,
+  Animated,
 } from "react-native";
+
+const data = [
+  { key: "1", title: "Step 1", description: "This is the first step." },
+  { key: "2", title: "Step 2", description: "This is the second step." },
+  { key: "3", title: "Step 3", description: "This is the third step." },
+  { key: "4", title: "Step 4", description: "This is the fourth step." },
+  { key: "5", title: "Step 5", description: "This is the fifth step." },
+];
+
+const TimelineItem = ({ item }) => (
+  <View style={styles.itemContainer}>
+    {/* <View style={styles.line} />
+    <View style={styles.circle} /> */}
+    <Text style={styles.title}>{item.title}</Text>
+    <Text style={styles.description}>{item.description}</Text>
+  </View>
+);
+
+const HorizontalTimeline = () => {
+  return (
+    // <SafeAreaView style={styles.timeLineContainer}>
+    <SafeAreaView>
+      <FlatList
+        data={data}
+        renderItem={({ item }) => <TimelineItem item={item} />}
+        keyExtractor={(item) => item.key}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.contentContainer}
+      />
+    </SafeAreaView>
+  );
+};
 
 const Brew = () => {
   const navigation = useNavigation();
 
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <SafeAreaView
+      style={[styles.container, { flex: 1, backgroundColor: COLORS.beige }]}
+    >
       <ScrollView contentContainerStyle={styles.container}>
         {/* Return Button */}
         <TouchableOpacity
@@ -36,6 +72,12 @@ const Brew = () => {
         >
           Currently brewing:
         </Text>
+        <Text style={styles.instructions}> Beer name: </Text>
+        <Text style={styles.instructions}> Current step: </Text>
+        <Text style={styles.instructions}> Current temperature: </Text>
+        <Text style={styles.instructions}> Remaining step time: </Text>
+
+        <HorizontalTimeline />
       </ScrollView>
     </SafeAreaView>
   );
--- Front-End/screens/brew/brew.style.js
+++ Front-End/screens/brew/brew.style.js
@@ -1,5 +1,9 @@
 import { StyleSheet } from "react-native";
 import { COLORS, FONT, SIZES } from "../../constants";
+import { Dimensions } from "react-native";
+
+const { width } = Dimensions.get("window");
+const { height } = Dimensions.get("window");
 
 const styles = StyleSheet.create({
   container: {
@@ -15,6 +19,13 @@ const styles = StyleSheet.create({
     marginTop: 1,
     textAlign: "center",
   },
+  instructions: {
+    fontFamily: FONT.bold,
+    fontSize: SIZES.mediumLarge,
+    color: COLORS.primary,
+    marginTop: SIZES.large,
+    textAlign: "left",
+  },
   returnButton: {
     position: "absolute",
     top: 5,
@@ -26,6 +37,57 @@ const styles = StyleSheet.create({
     fontFamily: FONT.bold,
     fontSize: SIZES.medium,
     color: COLORS.darkOrange,
+  },
+  // timeLineContainer: {
+  //   flex: 1,
+  //   justifyContent: "center",
+  //   alignItems: "center",
+  //   backgroundColor: COLORS.beige,
+  // },
+  contentContainer: {
+    paddingVertical: 20,
+  },
+  itemContainer: {
+    width: width * 0.8,
+    height: height * 0.2,
+    marginHorizontal: 10,
+    padding: 20,
+    backgroundColor: COLORS.lightWhite,
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    elevation: 5,
+  },
+  // line: {
+  //   position: "absolute",
+  //   left: 0,
+  //   top: "50%",
+  //   height: 2,
+  //   width: "100%",
+  //   backgroundColor: "#ddd",
+  // },
+  // circle: {
+  //   position: "absolute",
+  //   left: -10,
+  //   top: "50%",
+  //   marginTop: -10,
+  //   width: 20,
+  //   height: 20,
+  //   borderRadius: 10,
+  //   backgroundColor: "#3498db",
+  //   borderColor: "#fff",
+  //   borderWidth: 2,
+  // },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  description: {
+    fontSize: 14,
+    color: "#666",
   },
 });
 
