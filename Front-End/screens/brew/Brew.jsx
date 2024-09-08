fbed5b7806ccf351da3bd56841666b98e4d400ab
Adi Kapuri <adikap1904@gmail.com>
2024-09-08 21:35:05 +0300

    Brew fix

--- Front-End/screens/brew/Brew.jsx
+++ Front-End/screens/brew/Brew.jsx
@@ -14,60 +14,241 @@ import {
 } from "react-native";
 import { ID_TOKEN } from "../../utils/idToken.js";
 
-const data = [
-  {
-    key: "1",
-    title: "Step 1 - Brew Day",
-    description: "In process...",
-  },
-  {
-    key: "2",
-    title: "Step 2 - Fermentation",
-    description: "Not started yet",
-  },
-  {
-    key: "3",
-    title: "Step 3 - Bottling",
-    description: "Not started yet",
-  },
-];
+// const data = [
+//   {
+//     key: "1",
+//     title: "Step 1 - Brew Day",
+//     description: "In process...",
+//   },
+//   {
+//     key: "2",
+//     title: "Step 2 - Fermentation",
+//     description: "Not started yet",
+//   },
+//   {
+//     key: "3",
+//     title: "Step 3 - Bottling",
+//     description: "Not started yet",
+//   },
+// ];
 
-// const TimelineItem = ({ item }) => (
-//   <View style={styles.itemContainer}>
-//     <Text style={styles.title}>{item.title}</Text>
-//     <Text style={styles.description}>{item.description}</Text>
-//   </View>
-// );
+// const HorizontalTimeline = ({ stepNumber }) => {
+//   const updatedData = data.map((item) => {
+//     let newDescription = "Not started yet";
 
-const TimelineItem = ({ item }) => (
-  <View style={styles.itemContainer}>
-    <Text style={styles.title}>{item.title}</Text>
-    <View style={styles.descriptionContainer}>
-      <Text style={styles.description}>{item.description}</Text>
-      {item.description === "Done" && (
-        <Icon
-          name="check-circle"
-          size={24}
-          color="green"
-          style={styles.checkIcon}
-        />
-      )}
-    </View>
-  </View>
-);
+//     if (parseInt(item.key) < stepNumber) {
+//       newDescription = "Done";
+//     } else if (parseInt(item.key) === stepNumber) {
+//       newDescription = "In process";
+//     }
 
-const HorizontalTimeline = ({ stepNumber }) => {
-  const updatedData = data.map((item) => {
+//     return {
+//       ...item,
+//       description: newDescription,
+//     };
+//   });
+
+//   return (
+//     <SafeAreaView>
+//       <FlatList
+//         data={updatedData}
+//         renderItem={({ item }) => <TimelineItem item={item} />}
+//         keyExtractor={(item) => item.key}
+//         horizontal
+//         showsHorizontalScrollIndicator={false}
+//         contentContainerStyle={styles.contentContainer}
+//       />
+//     </SafeAreaView>
+//   );
+// };
+
+// const Brew = () => {
+//   const navigation = useNavigation();
+//   const [brewData, setBrewData] = useState([]);
+//   const [recipeData, setRecipeData] = useState([]);
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     const fetchBrewData = async () => {
+//       try {
+//         // First GET request to fetch the latest brew data
+//         const response = await axios.get(
+//           `https://brewtothefuture.azurewebsites.net/api/brew/data/latest`,
+//           {
+//             headers: {
+//               Authorization: `Bearer ${ID_TOKEN}`,
+//             },
+//           }
+//         );
+
+//         const brewDataResponse = response.data;
+//         setBrewData(brewDataResponse); // Set brewData after the first request completes
+
+//         // Check if recipe_id exists before making the second request
+//         if (brewDataResponse.recipe_id) {
+//           // Second GET request to fetch recipe data using recipe_id from the first response
+//           const recipeResponse = await axios.get(
+//             `https://brewtothefuture.azurewebsites.net/api/brew/recipe/id/${brewDataResponse.recipe_id}`,
+//             {
+//               headers: {
+//                 Authorization: `Bearer ${ID_TOKEN}`, // Include the token in the headers
+//               },
+//             }
+//           );
+
+//           setRecipeData(recipeResponse.data); // Set the fetched recipe data
+//         }
+//       } catch (error) {
+//         console.error("Error fetching brew or recipe data:", error);
+//       } finally {
+//         setLoading(false); // Stop loading after both requests are completed
+//       }
+//     };
+
+//     fetchBrewData();
+//   }, []);
+
+//   // useEffect(() => {
+//   //   // Simulate temperature change
+//   //   const interval = setInterval(() => {
+//   //     setTemperature((prevTemp) => (prevTemp >= 100 ? 0 : prevTemp + 10));
+//   //   }, 1000);
+
+//   //   return () => clearInterval(interval);
+//   // }, []);
+
+//   useEffect(() => {
+//     if (!brewData) {
+//       const interval = setInterval(() => {
+//         setBrewData((prevData) =>
+//           prevData
+//             ? {
+//                 ...prevData,
+//                 temperature_celsius: prevData.temperature_celsius,
+//               }
+//             : null
+//         );
+//       }, 1000);
+
+//       return () => clearInterval(interval);
+//     }
+//   }, []);
+
+//   if (loading) {
+//     return (
+//       <SafeAreaView style={styles.container}>
+//         <Text style={styles.loadingText}>Loading...</Text>
+//       </SafeAreaView>
+//     );
+//   }
+
+//   // if (!brewData) {
+//   //   return (
+//   //     <SafeAreaView style={styles.container}>
+//   //       <Text>No data available</Text>
+//   //     </SafeAreaView>
+//   //   );
+//   // }
+
+//   const calculateTimeDifference = (stepStartTime) => {
+//     const currentTime = Date.now();
+//     const timeDifference = currentTime - stepStartTime;
+
+//     const date = new Date(timeDifference);
+
+//     // Extract hours and minutes from the Date object
+//     const hours = date.getUTCHours(); // Use getUTCHours to handle the difference correctly
+//     const minutes = date.getUTCMinutes(); // Use getUTCMinutes for consistent time
+
+//     // Return the formatted time as a string
+//     return `${hours}h ${minutes}m`;
+//   };
+
+//   return (
+//     <SafeAreaView style={styles.container}>
+//       <ScrollView contentContainerStyle={styles.scrollContainer}>
+//         {/* Return Button */}
+//         <TouchableOpacity
+//           style={styles.returnButton}
+//           onPress={() => navigation.goBack()}
+//         >
+//           <Text style={styles.returnButtonText}>{"< Back"}</Text>
+//         </TouchableOpacity>
+//         <Text
+//           style={[
+//             styles.welcomeMessage,
+//             { marginTop: 30 },
+//             { marginBottom: 30 },
+//           ]}
+//         >
+//           Currently Brewing:
+//         </Text>
+//         {brewData != null ? (
+//           <>
+//             <View style={styles.dataContainer}>
+//               <Text style={styles.instructions}>
+//                 Beer name: {recipeData.recipe_name}
+//               </Text>
+//             </View>
+//             <View style={styles.dataContainer}>
+//               <Text style={styles.instructions}>
+//                 Current step: {brewData.current_step}
+//               </Text>
+//             </View>
+//             <View style={styles.dataContainer}>
+//               <Text style={styles.instructions}>
+//                 Time passed: {calculateTimeDifference(brewData.step_start_time)}
+//               </Text>
+//             </View>
+//             <Text style={styles.instructions}> Current temperature: </Text>
+
+//             <TemperatureBar
+//               temperature={brewData.temperature_celsius.toFixed(2)}
+//             />
+
+//             <View style={styles.stopButtonContainer}>
+//               <TouchableOpacity style={styles.stopButton}>
+//                 <Text style={styles.stopButtonText}>STOP BREWING</Text>
+//               </TouchableOpacity>
+//             </View>
+//             <HorizontalTimeline stepNumber={brewData.current_step} />
+//           </>
+//         ) : (
+//           <View style={styles.emptyTextContainer}>
+//             <Text style={styles.emptyText}>
+//               There is no brewing session occurring right now
+//             </Text>
+//             <HorizontalTimeline stepNumber={0} />
+//           </View>
+//         )}
+//       </ScrollView>
+//     </SafeAreaView>
+//   );
+// };
+
+// export default Brew;
+
+const HorizontalTimeline = ({ stepNumber, recipe }) => {
+  const updatedData = recipe.map((item, index) => {
     let newDescription = "Not started yet";
 
-    if (parseInt(item.key) < stepNumber) {
+    if (index + 1 < stepNumber) {
       newDescription = "Done";
-    } else if (parseInt(item.key) === stepNumber) {
+    } else if (index + 1 === stepNumber) {
       newDescription = "In process";
     }
 
+    let newTitle;
+
+    if (item.message == null) {
+      newTitle = `Step ${index + 1} - Temperature Rest`;
+    } else {
+      newTitle = `Step ${index + 1} - ${item.message}`;
+    }
+
     return {
-      ...item,
+      key: (index + 1).toString(),
+      title: newTitle,
       description: newDescription,
     };
   });
@@ -86,67 +267,32 @@ const HorizontalTimeline = ({ stepNumber }) => {
   );
 };
 
+const TimelineItem = ({ item }) => (
+  <View style={styles.itemContainer}>
+    <Text style={styles.title}>{item.title}</Text>
+    <View style={styles.descriptionContainer}>
+      <Text style={styles.description}>{item.description}</Text>
+      {item.description === "Done" && (
+        <Icon
+          name="check-circle"
+          size={24}
+          color="green"
+          style={styles.checkIcon}
+        />
+      )}
+    </View>
+  </View>
+);
+
 const Brew = () => {
   const navigation = useNavigation();
-  // const route = useRoute();
-  // const { recipeId } = route.params;
-  const [brewData, setBrewData] = useState([]);
-  const [recipeData, setRecipeData] = useState([]);
+  const [brewData, setBrewData] = useState();
+  const [recipeData, setRecipeData] = useState();
   const [loading, setLoading] = useState(true);
-
-  // useEffect(() => {
-  //   const fetchBrewData = async () => {
-  //     try {
-  //       const response = await axios.get(
-  //         `https://brewtothefuture.azurewebsites.net/api/brew/data/latest`,
-  //         {
-  //           headers: {
-  //             Authorization: `Bearer ${ID_TOKEN}`,
-  //           },
-  //         }
-  //       );
-
-  //       setBrewData(response.data);
-  //     } catch (error) {
-  //       console.log("Error fetching brew data:", error);
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   };
-
-  //   fetchBrewData();
-  //   // }, [userId]);
-  // }, []);
-
-  // useEffect(() => {
-  //   console.log(brewData.recipe_id);
-  //   // Fetch the data from the API
-  //   const fetchBrewData = async () => {
-  //     try {
-  //       const response = await axios.get(
-  //         `https://brewtothefuture.azurewebsites.net/api/brew/recipe/id/${brewData.recipe_id}`,
-  //         {
-  //           headers: {
-  //             Authorization: `Bearer ${ID_TOKEN}`, // Include the token in the headers
-  //           },
-  //         }
-  //       );
-
-  //       setRecipeData(response.data); // Save the fetched data to state
-  //     } catch (error) {
-  //       console.error("Error fetching recipe data:", error);
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   };
-
-  //   fetchBrewData();
-  // }, []);
 
   useEffect(() => {
     const fetchBrewData = async () => {
       try {
-        // First GET request to fetch the latest brew data
         const response = await axios.get(
           `https://brewtothefuture.azurewebsites.net/api/brew/data/latest`,
           {
@@ -157,73 +303,29 @@ const Brew = () => {
         );
 
         const brewDataResponse = response.data;
-        setBrewData(brewDataResponse); // Set brewData after the first request completes
+        setBrewData(brewDataResponse);
 
-        // Check if recipe_id exists before making the second request
         if (brewDataResponse.recipe_id) {
-          // Second GET request to fetch recipe data using recipe_id from the first response
           const recipeResponse = await axios.get(
             `https://brewtothefuture.azurewebsites.net/api/brew/recipe/id/${brewDataResponse.recipe_id}`,
             {
               headers: {
-                Authorization: `Bearer ${ID_TOKEN}`, // Include the token in the headers
+                Authorization: `Bearer ${ID_TOKEN}`,
               },
             }
           );
 
-          setRecipeData(recipeResponse.data); // Set the fetched recipe data
+          setRecipeData(recipeResponse.data);
         }
       } catch (error) {
         console.error("Error fetching brew or recipe data:", error);
       } finally {
-        setLoading(false); // Stop loading after both requests are completed
+        setLoading(false);
       }
     };
 
     fetchBrewData();
   }, []);
-
-  // useEffect(() => {
-  //   // Simulate temperature change
-  //   const interval = setInterval(() => {
-  //     setTemperature((prevTemp) => (prevTemp >= 100 ? 0 : prevTemp + 10));
-  //   }, 1000);
-
-  //   return () => clearInterval(interval);
-  // }, []);
-
-  useEffect(() => {
-    if (!brewData) {
-      const interval = setInterval(() => {
-        setBrewData((prevData) =>
-          prevData
-            ? {
-                ...prevData,
-                temperature_celsius: prevData.temperature_celsius,
-              }
-            : null
-        );
-      }, 1000);
-
-      return () => clearInterval(interval);
-    }
-  }, []);
-
-  if (loading) {
-    return (
-      <SafeAreaView style={styles.container}>
-        <Text style={styles.loadingText}>Loading...</Text>
-      </SafeAreaView>
-    );
-  }
-
-  // if (!brewData) {
-  //   return (
-  //     <SafeAreaView style={styles.container}>
-  //       <Text>No data available</Text>
-  //     </SafeAreaView>
-  //   );
-  // }
 
   const calculateTimeDifference = (stepStartTime) => {
     const currentTime = Date.now();
@@ -238,6 +340,14 @@ const Brew = () => {
     // Return the formatted time as a string
     return `${hours}h ${minutes}m`;
   };
+
+  if (loading) {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.loadingText}>Loading...</Text>
+      </SafeAreaView>
+    );
+  }
 
   return (
     <SafeAreaView style={styles.container}>
@@ -276,24 +386,43 @@ const Brew = () => {
               </Text>
             </View>
             <Text style={styles.instructions}> Current temperature: </Text>
-
             <TemperatureBar
               temperature={brewData.temperature_celsius.toFixed(2)}
             />
-
             <View style={styles.stopButtonContainer}>
               <TouchableOpacity style={styles.stopButton}>
                 <Text style={styles.stopButtonText}>STOP BREWING</Text>
               </TouchableOpacity>
             </View>
-            <HorizontalTimeline stepNumber={brewData.current_step} />
+            {console.log(recipeData.recipe)}
+
+            <HorizontalTimeline
+              stepNumber={brewData.current_step}
+              recipe={recipeData.recipe}
+            />
           </>
         ) : (
           <View style={styles.emptyTextContainer}>
             <Text style={styles.emptyText}>
               There is no brewing session occurring right now
             </Text>
-            <HorizontalTimeline stepNumber={0} />
+            <HorizontalTimeline
+              stepNumber={0}
+              recipe={[
+                {
+                  step_id: 1,
+                  message: "Brew Day",
+                },
+                {
+                  step_id: 2,
+                  message: "Fermentation",
+                },
+                {
+                  step_id: 3,
+                  message: "Bottling",
+                },
+              ]}
+            />
           </View>
         )}
       </ScrollView>
